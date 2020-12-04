19ba2667c6f9630302e58df52655ee26a48108e5
seoquesto <seoquesto@gmail.com>
2020-12-04 21:46:17 +0100

    Sample

---
+++ src/server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Docker');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
